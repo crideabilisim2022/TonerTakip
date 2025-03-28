28d7791d54acd557524bc07578c8722d26860548
Berat Dimen <beratefedimen@hotmail.com>
2025-03-28 11:41:19 +0300

    footer ile header arası açıldı

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,8 +22,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header />
-        {children}
+        <div className="generalBody">
+          <Header />
+          <div className="general">{children}</div>
+
+          <Footer />
+        </div>
       </body>
     </html>
   );
---
+++ components/footer/index.jsx
@@ -0,0 +1,42 @@
+import { InstagramIcon, LinkedinIcon, WhatsappIcon } from "@/helpers/icons";
+import "./style.css";
+
+export default function Footer() {
+  return (
+    <>
+      {/* <div className="footerContainer"> */}
+      {/* <ul>
+          <h5>About</h5>
+          <li>Company</li>
+          <li>Comunutiy</li>
+          <li>Careers</li>
+        </ul>
+        <ul>
+          <h5>Blog</h5>
+          <li>Company</li>
+          <li>Comunutiy</li>
+          <li>Careers</li>
+        </ul>
+        <ul>
+          <h5>Product</h5>
+          <li>Company</li>
+          <li>Comunutiy</li>
+          <li>Careers</li>
+        </ul>
+        <div className="logo">
+          <h5>Cridea</h5>
+          <div className="social">
+            <InstagramIcon />
+            <LinkedinIcon />
+            <WhatsappIcon />
+          </div>
+        </div>
+      </div> */}
+
+      <div className="altFooter">
+        <p>24 Atlantic Ave , Broklyn, NY 11201</p>
+        <p>c 2016 Dreamy Inc. All rights reserved</p>
+      </div>
+    </>
+  );
+}
